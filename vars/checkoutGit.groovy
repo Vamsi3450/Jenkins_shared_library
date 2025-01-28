2e1e318088db963a4c6b4358a51b2e0a97b3303f
VAMSI <144320777+Vamsi3450@users.noreply.github.com>
2025-01-28 15:32:20 +0530

    Update checkoutGit.groovy

--- vars/checkoutGit.groovy
+++ vars/checkoutGit.groovy
@@ -1,7 +1,7 @@
 def call(String gitUrl, String gitBranch) {
     checkout([
         $class: 'GitSCM',
-        branches: [[name: gitBranch]],
-        userRemoteConfigs: [[url: gitUrl]]
+        branches: [[name: '*/main']],
+        userRemoteConfigs: [[url: 'https://github.com/Vamsi3450/Jenkins_shared_library.git']]
     ])
 }
